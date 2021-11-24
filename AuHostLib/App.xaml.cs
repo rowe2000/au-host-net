3d1f68b640821948ac6ffd41cfd60fb90bd7d877
rowe2000 <robert@westman.st>
2021-11-24 10:59:14 +0100

    added files

---
+++ AuHostLib/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AuHost.Pages;
+using AuHost.Plugins;
+using Xamarin.Forms;
+
+namespace AuHost
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            PluginGraph.Instance.Load();
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
+
---
+++ AuHostLib/Pages/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AuHost.ViewModels;
+using Xamarin.Forms;
+
+namespace AuHost.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
+
